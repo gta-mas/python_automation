7097f63afabf09d5126151a5dbc426c1e8d85fbc
gtamas <gabriel.tamas11@proton.me>
2023-11-23 18:05:48 +0100

    added command line options to select specific browsers (-invocations, too lazy for that, sticking to chrome)

--- pytestss_demo/conftest.py
+++ pytestss_demo/conftest.py
@@ -27,6 +27,32 @@ def load_data():
 def cross_browser(request):
     return request.param
 
+#command line hook to select specific browser/default to a specific browser
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--browser_name", action="store", default="chrome"
+#     )
+=======================================================================================
+#commands to setup a command line options for browser selection:
+#browser_name = request.config.getOption("browser_name")
+#     if browser_name == "chrome":
+#         chrome_options = Options()
+#         chrome_options.add_argument("--disable-exit-after-done")
+#         chrome_options.add_argument("--start-maximized")
+#
+#         service_obj = Service("D:/Software Tester/chromedriver.exe")
+#         driver = webdriver.Chrome(service=service_obj, options=chrome_options)
+#         driver.implicitly_wait(5)
+#
+#     elif browser_name == "firefox":
+#         #firefox invocation
+#     elif browser_name == "IE":
+#         #IE invocation
+#
+#     driver.get("https://rahulshettyacademy.com/angularpractice/")
+#     driver.maximize_window()
+======================================================================================
+
 
 @pytest.fixture(scope="class")
 def setup(request):
