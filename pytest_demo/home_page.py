13d6463226cf79a7f8f4bd71db323483846364ce
gta-mas <gabriel.tamas11@proton.me>
2023-12-01 19:09:03 +0100

    test case 2

--- pytest_demo/home_page.py
+++ pytest_demo/home_page.py
@@ -4,12 +4,19 @@ from pytest_demo.checkout_page import CheckoutPage
 
 
 class HomePage:
-    #constructor accepts argument to form a connection with this local driver
-    #setting up test case driver to local driver
+    # constructor accepts argument to form a connection with this local driver
+    # setting up test case driver to local driver
     def __init__(self, driver):
         self.driver = driver
 
     shop = (By.CSS_SELECTOR, "a[href*='shop']")
+    name = (By.CSS_SELECTOR, "input[name='name']")
+    email = (By.NAME, "email")
+    password = (By.ID, "exampleInputPassword1")
+    checkbox = (By.ID, "exampleCheck1")
+    gender = (By.ID, "exampleFormControlSelect1")
+    submit = (By.XPATH, "//input[@type='submit']")
+    success_message = (By.CLASS_NAME, "alert-success")
 
     def shopItems(self):
         self.driver.find_element(*HomePage.shop).click()
@@ -17,4 +24,31 @@ class HomePage:
         checkout_page = CheckoutPage(self.driver)
         return checkout_page
 
+    def getName(self):
+        return self.driver.find_element(*HomePage.name)
+#       driver.find_element(By.CSS_SELECTOR, "input[name='name']")
+
+    def getEmail(self):
+        return self.driver.find_element(*HomePage.email)
+#       driver.find_element(By.NAME, "email")
+
+    def getPassword(self):
+        return self.driver.find_element(*HomePage.password)
+#       driver.find_element(By.ID, "exampleInputPassword1")
+
+    def checkBox(self):
+        return self.driver.find_element(*HomePage.checkbox)
+#       driver.find_element(By.ID, "exampleCheck1")
+
+    def getGender(self):
+        return self.driver.find_element(*HomePage.gender)
+#       driver.find_element(By.ID, "exampleFormControlSelect1"))
+
+    def submitButton(self):
+        return self.driver.find_element(*HomePage.submit)
+#       driver.find_element(By.XPATH, "//input[@type='submit']")
+
+    def getSuccess(self):
+        return self.driver.find_element(*HomePage.success_message)
+#       driver.find_element(By.CLASS_NAME, "alert-success")
 
--- pytest_demo/test_home_page.py
+++ pytest_demo/test_home_page.py
@@ -2,37 +2,50 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-class TestHomePage:
+from pytest_demo.base_class import BaseClass
+from pytest_demo.home_page import HomePage
+
+
+class TestHomePage(BaseClass):
 
     def test_form_submission(self):
 
-        service_obj = Service("D:/Software Testing/chromedriver.exe")
-        driver = webdriver.Chrome(service=service_obj)
-
-        driver.get("https://rahulshettyacademy.com/angularpractice/")
+        # service_obj = Service("D:/Software Testing/chromedriver.exe")
+        # driver = webdriver.Chrome(service=service_obj)
+        # driver.get("https://rahulshettyacademy.com/angularpractice/")
+        # driver.maximize_window()
 
         # ID, Xpath, CSS Selector, Classname, name, linkText
         # CSS Selector formula: tagname[attribute="attribute"], #id, .classname
         # Xpath formula: //tagname[@attribute="attribute"]
-        driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Tvoja mac")
-        driver.find_element(By.NAME, "email").send_keys("abcd@gmail.com")
-        driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
-        driver.find_element(By.ID, "exampleCheck1").click()
+        # driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Tvoja mac")
+        home_page = HomePage(self.driver)
+        home_page.getName().send_keys("Tvoja mac")
+
+        # driver.find_element(By.NAME, "email").send_keys("abcd@gmail.com")
+        home_page.email().send_keys("abcd@gmail.com")
+
+        # driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
+        home_page.password().send_keys("123456")
+
+        # driver.find_element(By.ID, "exampleCheck1").click()
+        home_page.checkbox().click()
 
         # dropdown menu
-        dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+        dropdown = Select(home_page.gender().click())
         dropdown.select_by_visible_text("Male")
         time.sleep(2)
         dropdown.select_by_index(0)
         # dropdown.select_by_value("value_name")
         time.sleep(2)
 
-        driver.find_element(By.XPATH, "//input[@type='submit']").click()
+        #driver.find_element(By.XPATH, "//input[@type='submit']").click()
+        home_page.submit().click()
 
-        message = driver.find_element(By.CLASS_NAME, "alert-success").text
+        message = home_page.success_message().text
         print(message)
+
         assert "Success" in message
