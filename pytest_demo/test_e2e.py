3b467763a79bb8421982988ed36ffbf2ddc7ceea
gta-mas <gabriel.tamas11@proton.me>
2023-12-05 13:14:44 +0100

    adding loggers to the test

--- pytest_demo/test_e2e.py
+++ pytest_demo/test_e2e.py
@@ -18,16 +18,21 @@ from pytest_demo.home_page import HomePage
 class TestOne(BaseClass):
 
     def test_e2e(self):
+        log = self.get_logger()
+
         # //a[contains(@href,'shop')] - XPATH   a[href*='shop'] - CSS
         # self.driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
         home_page = HomePage(self.driver)
         checkout_page = home_page.shopItems()
+
+        log.info("Card Titles")
 
         # products = self.driver.find_elements(By.XPATH, "//div[@class='card h-100']")
         # checkout_page = CheckoutPage(self.driver) - cleaned up
         products = checkout_page.getCardTitle()
         for product in products:
             product_name = product.find_element(By.XPATH, "div/h4/a").text
+            log.info(product_name)
             if product_name == "Blackberry":
                 product.find_element(By.XPATH, "div/button").click()
 
@@ -38,6 +43,8 @@ class TestOne(BaseClass):
         # self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
         checkout_success = CheckoutPage(self.driver)
         confirm_page = checkout_success.checkoutSuccess()
+
+        log.info("Typing Country Name")
 
         # self.driver.find_element(By.ID, "country").send_keys("slov")
         get_country = ConfirmPage(self.driver)
@@ -60,6 +67,8 @@ class TestOne(BaseClass):
         submit_order.submit().click()
 
         success_text = self.driver.find_element(By.CLASS_NAME, "alert-success").text
+
+        log.info("Assertion successful: " + success_text)
 
         assert "Success! Thank you!" in success_text
 
